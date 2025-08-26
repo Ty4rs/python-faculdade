74f2454dfdd8c8d95ea2af231297ef381e99a885
ty4rs <joaovitordefreitasfelix@gmail.com>
2025-08-26 20:35:58 -0300

    aula 1.3

--- aula1-vetores.py
+++ aula1-vetores.py
@@ -17,3 +17,27 @@ print("Primeiro elemento de vet2:", vet2[0])  # Exibe o primeiro elemento
 print("Segundo elemento de vet2:", vet2[1])   # Exibe o segundo elemento
 
 type(vet2)  # Verifica o tipo da variável vet2 (deve ser 'list')
+
+
+
+vetor = [1, 2, 3, 4, 5, 6, 7] # Criação de uma lista com elementos iniciais
+vetor2 = [10, 20, 30, 40, 50, 60, 70] # Criação de uma segunda lista com elementos iniciais
+vetor.append(vetor2) # Adiciona a lista vetor2 como um único elemento no final da lista vetor
+print(vetor) # Exibe o conteúdo da lista vetor, que agora inclui vetor2 como um sub-lista
+print(vetor[7][5]) # Acessa e exibe o sexto elemento da sub-lista vetor2 dentro de vetor (índice 7)
+
+
+def oi():
+    return "oiii"
+
+
+vetor4 = [1, oi(), oi()]  # Criação de uma lista contendo o retorno da função oi() três vezes
+print(vetor4)  # Exibe o conteúdo da lista vetor4, que deve conter três vezes a string "oiii"
+
+def ola():
+    print("oii")        
+
+vetor5 = [1, 3, ola]
+print(vetor5)  # Exibe o conteúdo da lista vetor5, que inclui a função ola
+# Verificando o tipo do primeiro elemento da lista vetor
+vetor5[2]()
